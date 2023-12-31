f76f8b1d2f67737636a7878a3034ad453121d1e1
RizwanSuvo99 <rizwanshuvo@gmail.com>
2023-12-31 17:28:52 +0600

    "Hero Section Added"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,13 @@ import heroImg from "../../assets/img/hero__img.png";
 const Home = () => {
   return (
     <div className="home flex">
-      <div className="home__text">
+      <div className="home__text flex">
         <h1>Empower & Elevate</h1>
         <h1>Explore Jobs with Halal Jibika</h1>
         <p>Unlocking Doors to Exciting Opportunities and Growth</p>
+        <div>
+          <button className="btn">Explore All Jobs</button>
+        </div>
       </div>
       <div className="home__img">
         <img src={heroImg} alt="hero-bg" />
