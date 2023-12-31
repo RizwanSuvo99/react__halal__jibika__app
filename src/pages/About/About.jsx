58ab30742e9265b35e99ecd7ccdf7bf5dcd4cbce
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-01 01:48:23 +0600

    "About Pages Added"

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,46 @@
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+// AboutPage.js
+import "./About.css";
 
-export default About
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <div className="about__text">
+        <div className="company-info">
+          <h1>Welcome to HALAL JIBIKA</h1>
+          <p>
+            “HALAL JIBIKA" is more than just a job portal; it's a platform
+            designed to simplify the job application process and empower
+            individuals in their career pursuits.
+          </p>
+          <p>
+            Whether you're a seasoned professional or just starting your career
+            journey, HALAL JIBIKA offers a diverse range of job opportunities to
+            explore.
+          </p>
+          <p>
+            Our vision is rooted in the belief that success comes to those who
+            persevere. "Keep hardworking, practice patience, and stay prepared
+            for opportunities."
+          </p>
+        </div>
+        <div className="additional-details">
+          <h1>Why Choose HALAL JIBIKA?</h1>
+          <ul>
+            <li>Wide Range of Job Opportunities</li>
+            <li>Simple and Efficient Application Process</li>
+            <li>Empowering Individuals for Career Growth</li>
+            <li>User-Friendly Registration and Login System</li>
+          </ul>
+        </div>
+      </div>
+      <div className="about__img">
+        <img
+          src="https://img.freepik.com/free-photo/customer-service-cute-brunette-guy-suit-with-laptop-happy-touching-chin_140725-164987.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,27 @@ import heroImg from "../../assets/img/hero__img.png";
 
 const Home = () => {
   return (
-    <div className="home flex">
-      <div className="home__text flex">
-        <h1>Empower & Elevate</h1>
-        <h1>Explore Jobs with Halal Jibika</h1>
-        <p>Unlocking Doors to Exciting Opportunities and Growth</p>
-        <div>
-          <button className="btn">Explore All Jobs</button>
+    <>
+      <div className="home flex">
+        <div className="home__text flex">
+          <h1>Empower & Elevate</h1>
+          <h1>Explore Jobs with Halal Jibika</h1>
+          <p>Unlocking Doors to Exciting Opportunities and Growth</p>
+          <div>
+            <button className="btn">Explore All Jobs</button>
+          </div>
+        </div>
+        <div className="home__img">
+          <img src={heroImg} alt="hero-bg" />
         </div>
       </div>
-      <div className="home__img">
-        <img src={heroImg} alt="hero-bg" />
+      <div className="hero__bottom">
+        <div className="user__card"></div>
+        <div className="user__card"></div>
+        <div className="user__card"></div>
+        <div className="user__card"></div>
       </div>
-    </div>
+    </>
   );
 };
 
